74e24614b9d7233f096539bfdf0d47e5cad2d7de
ishak <ishaksebsib@gmail.com>
2024-08-31 09:57:08 +0300

    [ENH] basic health check api with initial setup

---
+++ conversational-rag/src/server.ts
@@ -0,0 +1,24 @@
+import cors from "cors";
+import express, { type Express } from "express";
+import { pino } from "pino";
+import { env } from "process";
+import { healthCheckRouter } from "./api/healthCheckRouter";
+
+const logger = pino({ name: "server start" });
+const app: Express = express();
+
+// connect to the database
+//connectDB();
+
+// Set the application to trust the reverse proxy
+app.set("trust proxy", true);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+
+// Routes
+app.use("/", healthCheckRouter);
+
+export { app, logger };
