f198e30b4a219cd5bb18462a75f8e2579af25fe9
ishaksebsib <ishaksebsib@gmail.com>
2024-09-07 11:51:29 +0300

    [ENH] handle error on /chat quesion

--- conversational-rag/src/api/chat/chatController.ts
+++ conversational-rag/src/api/chat/chatController.ts
@@ -3,11 +3,17 @@ import ChatService from "./chatService";
 
 class ChatController {
 	public async getAnswer(req: Request, res: Response) {
+		const { quesstion: question } = req.body;
+
+		if (!question) {
+			return res.status(500).json({ error: "quesstion required!" })
+		}
+
 		try {
 			const chatService = new ChatService(
-				"https://en.wikipedia.org/wiki/Barack_Obama",
+				"https://www.britannica.com/biography/Abiy-Ahmed",
 			);
-			const response = await chatService.askQuestion(req.body.question);
+			const response = await chatService.askQuestion(question);
 			res.status(200).json(response);
 		} catch (error: any) {
 			res.status(500).json({ error: error.message });
