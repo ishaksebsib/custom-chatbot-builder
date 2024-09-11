33b8588d7de971fad190cd40b80c845238176693
ishaksebsib <ishaksebsib@gmail.com>
2024-09-11 12:31:53 +0300

    [ENH]: ADD grace full shutdown for mongodb connection and hanlde mogodb connection events

--- conversational-rag/src/db/connect.ts
+++ conversational-rag/src/db/connect.ts
@@ -2,15 +2,70 @@ import { env } from "@/common/utils/envConfig";
 import { logger } from "@/server";
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(env.MONGO_URI);
+class MongoDBConnection {
+	private mongoURL: string;
+	private connection: typeof mongoose | null;
 
-		logger.info(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error: any) {
-		logger.error(`Error: ${error.message}`);
-		process.exit(1);
+	constructor() {
+		this.mongoURL = env.MONGO_URI;
+		this.connection = null;
 	}
-};
 
-export default connectDB;
+	// singleton pattern
+	async connect() {
+		if (this.connection) {
+			logger.info("Already connected to MongoDB");
+			return this.connection;
+		}
+
+		try {
+			this.handleConnectionEvents();
+			this.connection = await mongoose.connect(this.mongoURL);
+			return this.connection;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async disconnect() {
+		if (!this.connection) {
+			logger.info("Already disconnected from MongoDB");
+			return;
+		}
+
+		try {
+			await mongoose.disconnect();
+			this.connection = null;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async closeConnection() {
+		logger.info("Closing MongoDB connection");
+		try {
+			await this.disconnect();
+			logger.info("MongoDB connection closed");
+		} catch (error) {
+			logger.error("Error closing MongoDB connection", error);
+		}
+	}
+
+	handleConnectionEvents() {
+		const db = mongoose.connection;
+
+		db.once("open", () => {
+			logger.info("Connected to MongoDB");
+		});
+
+		db.on("disconnected", () => {
+			logger.info("Disconnected from MongoDB");
+		});
+
+		db.on("error", (error) => {
+			logger.error("Error connecting to MongoDB", error);
+		});
+	}
+}
+
+export const db = new MongoDBConnection();
