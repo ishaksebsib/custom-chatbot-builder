f242f357e148768c70b40f334f29280b790b3a6e
ishaksebsib <ishaksebsib@gmail.com>
2024-09-12 22:55:47 +0300

    [ENH] ajust reponse helper naming

--- src/common/utils/responseHelpers.ts
+++ src/common/utils/responseHelpers.ts
@@ -4,38 +4,41 @@ import { StatusCodes } from "http-status-codes";
 export class CreateResponse<T = null> {
   readonly success: boolean;
   readonly message: string;
-  readonly responseObject: T;
+  readonly response: T;
   readonly statusCode: number;
 
   private constructor(
     success: boolean,
     message: string,
-    responseObject: T,
+    response: T,
     statusCode: number,
   ) {
     this.success = success;
     this.message = message;
-    this.responseObject = responseObject;
+    this.response = response;
     this.statusCode = statusCode;
   }
 
   static success<T>(
     message: string,
-    responseObject: T,
+    response: T,
     statusCode: number = StatusCodes.OK,
   ) {
-    return new CreateResponse(true, message, responseObject, statusCode);
+    return new CreateResponse(true, message, response, statusCode);
   }
 
   static failure<T>(
     message: string,
-    responseObject: T,
+    response: T,
     statusCode: number = StatusCodes.BAD_REQUEST,
   ) {
-    return new CreateResponse(false, message, responseObject, statusCode);
+    return new CreateResponse(false, message, response, statusCode);
   }
 }
 
-export const sendResponse = <T>(res: Response, responseObject: CreateResponse<T>) => {
+export const sendResponse = <T>(
+  res: Response,
+  responseObject: CreateResponse<T>,
+) => {
   return res.status(responseObject.statusCode).send(responseObject);
 };
